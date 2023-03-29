1b7e2101640a3a9ec0f2cf2860d53552d99e6f09
Gaia Ducournau <gaia.ducournau@gmail.com>
2023-03-29 16:35:09 +0200

    ajout modération par rôle
    Co-Authored-By: chaton2a <112857321+chaton2a@users.noreply.github.com>

--- CodeSource/ArticlesAPI.php
+++ CodeSource/ArticlesAPI.php
@@ -4,74 +4,93 @@ require('lib.php');
 /// Paramétrage de l'entête HTTP (pour la réponse au Client)
 header("Content-Type:application/json");
 
+$role=extract_user_role(get_bearer_token());
+echo $role;
+if ($role = 'publisher') {
+    $publisher=extract_username(get_bearer_token());
+}
+
 /// Identification du type de méthode HTTP envoyée par le client
 $http_method = $_SERVER['REQUEST_METHOD'];
 switch ($http_method){
 
     /// Cas de la méthode POST
     case "POST" :
-        /// Récupération des données envoyées par le Client
-        $postedData = file_get_contents('php://input');
+        if ($role = 'publisher') {
+            /// Récupération des données envoyées par le Client
+            $postedData = file_get_contents('php://input');
+            if (!empty($_GET['Id_article'])){
 
-        /// Traitement
-        $blob=json_decode($postedData,true);
-        $Id_article = $blob['Id_article'];
-        $Date_publication = $blob['Date_publication'];
-        $Contenu = $blob['Contenu'];
-        $Publisher = $blob['Publisher'];
-        $requeteId_article = $linkpdo->prepare('SELECT * FROM article WHERE Id_article = :Id_article');
-        $requeteId_article->execute(array(':Id_article' => $Id_article));
-        $matchingData = $requeteId_article->fetchALL();
-        if(!$matchingData){
-            $req = $linkpdo->prepare('INSERT INTO article (Id_article,Date_publication,Contenu,Publisher) 
-            VALUES (:Id_article,:Date_publication,:Contenu,:Publisher)');
-            $req->execute(array('Id_article' => $Id_article,'Date_publication' => $Date_publication,'Contenu' => $Contenu, 'Publisher' => $Publisher));    
-             deliver_response(200, "OK : données ajoutées", NULL);
+            }
+            /// Traitement
+            $blob=json_decode($postedData,true);
+            $Id_article = $blob['Id_article'];
+            $Date_publication = $blob['Date_publication'];
+            $Contenu = $blob['Contenu'];
+            $requeteId_article = $linkpdo->prepare('SELECT * FROM article WHERE Id_article = :Id_article');
+            $requeteId_article->execute(array(':Id_article' => $Id_article));
+            $matchingData = $requeteId_article->fetchALL();
+            if(!$matchingData){
+                $req = $linkpdo->prepare('INSERT INTO article (Id_article,Date_publication,Contenu,Publisher) 
+                VALUES (:Id_article,:Date_publication,:Contenu,:Publisher)');
+                $req->execute(array('Id_article' => $Id_article,'Date_publication' => $Date_publication,'Contenu' => $Contenu, 'Publisher' => $Publisher));    
+                deliver_response(200, "OK : données ajoutées", NULL);
+            }else{
+                deliver_response(401, "Error : Identifiant déjà existant", NULL);
+            }
         }else{
-            deliver_response(401, "Error : Identifiant déjà existant", NULL);
-        }
-
-    /// Cas de la méthode PUT
-    case "PUT" :
-        /// Récupération des données envoyées par le Client
-        $postedData = file_get_contents('php://input');
-
-        /// Traitement
-        $blob=json_decode($postedData,true);
-        $Id_article = $blob['Id_article'];
-        $Date_publication = $blob['Date_publication'];
-        $Contenu = $blob['Contenu'];
-        $Publisher = $blob['Publisher'];
-        $requeteId_article = $linkpdo->prepare('SELECT * FROM article WHERE Id_article = :Id_article');
-        $requeteId_article->execute(array(':Id_article' => $Id_article));
-        $matchingData = $requeteId_article->fetchALL();
-        if($matchingData){
-            $req = $linkpdo->prepare('UPDATE article set Date_publication = :Date_publication, Contenu = :Contenu, 
-            Publisher = :Publisher where Id_article = :Id_article');
-            $req->execute(array('Date_publication' => $Date_publication,'Contenu' => $Contenu,
-             'Publisher' => $Publisher,'Id_article' => $Id_article));
-             deliver_response(200, "OK : données mises à jour", NULL);
-        }else{
-            deliver_response(401, "Error : Pas d'article trouvé pour l'id donné", NULL);
+            deliver_response(401, "Error : Vous n'avez pas le role necessaire pour cette méthode", NULL);
         }
         break;
 
-    case "DELETE" : 
-        /// Récupération des critères de recherche envoyés par le Client
-        if (!empty($_GET['Id_article'])){
-            $Id_article=$_GET['Id_article'];
+    /// Cas de la méthode PUT
+    case "PUT" :
+        if ($role = 'publisher') {
+            /// Récupération des données envoyées par le Client
+            $postedData = file_get_contents('php://input');
+
+            /// Traitement
+            $blob=json_decode($postedData,true);
+            $Id_article = $blob['Id_article'];
+            $Date_publication = $blob['Date_publication'];
+            $Contenu = $blob['Contenu'];
             $requeteId_article = $linkpdo->prepare('SELECT * FROM article WHERE Id_article = :Id_article');
             $requeteId_article->execute(array(':Id_article' => $Id_article));
             $matchingData = $requeteId_article->fetchALL();
             if($matchingData){
-                $requeteId_article = $linkpdo->prepare('DELETE FROM article WHERE Id_article = :Id_article');
-                $requeteId_article->execute(array(':Id_article' => $Id_article));
-                deliver_response(200, "OK : article supprimé", NULL);
+                $req = $linkpdo->prepare('UPDATE article set Date_publication = :Date_publication, Contenu = :Contenu, 
+                Publisher = :Publisher where Id_article = :Id_article');
+                $req->execute(array('Date_publication' => $Date_publication,'Contenu' => $Contenu,
+                'Publisher' => $Publisher,'Id_article' => $Id_article));
+                deliver_response(200, "OK : données mises à jour", NULL);
             }else{
                 deliver_response(401, "Error : Pas d'article trouvé pour l'id donné", NULL);
             }
         }else{
-            deliver_response(400, "Error : DELETE ne fonctionne pas sans identifiant", NULL);
+            deliver_response(401, "Error : Vous n'avez pas le role necessaire pour cette méthode", NULL);
+        }
+        break;
+
+    case "DELETE" : 
+        if ($role = 'publisher' or $role = 'moderator' ) {
+            /// Récupération des critères de recherche envoyés par le Client
+            if (!empty($_GET['Id_article'])){
+                $Id_article=$_GET['Id_article'];
+                $requeteId_article = $linkpdo->prepare('SELECT * FROM article WHERE Id_article = :Id_article');
+                $requeteId_article->execute(array(':Id_article' => $Id_article));
+                $matchingData = $requeteId_article->fetchALL();
+                if($matchingData){
+                    $requeteId_article = $linkpdo->prepare('DELETE FROM article WHERE Id_article = :Id_article');
+                    $requeteId_article->execute(array(':Id_article' => $Id_article));
+                    deliver_response(200, "OK : article supprimé", NULL);
+                }else{
+                    deliver_response(401, "Error : Pas d'article trouvé pour l'id donné", NULL);
+                }
+            }else{
+                deliver_response(400, "Error : DELETE ne fonctionne pas sans identifiant", NULL);
+            }
+        }else{
+            deliver_response(401, "Error : Vous n'avez pas le role necessaire pour cette méthode", NULL);
         }
         break;
 
@@ -80,13 +99,26 @@ switch ($http_method){
         /// Récupération des critères de recherche envoyés par le Client
         if (!empty($_GET['Id_article'])){
             $Id_article=$_GET['Id_article'];
-            $requeteId_article = $linkpdo->prepare('SELECT * FROM article WHERE Id_article = :Id_article');
+            if ($role = 'publisher' or $role = 'moderator') {
+                $requeteId_article = $linkpdo->prepare('select COUNT(Est_like) as Nombre_like, article.* 
+                                                        FROM interagir , article 
+                                                        where interagir.Id_article = article.Id_article 
+                                                        and est_like = 1');
+            }else{
+                $requeteId_article = $linkpdo->prepare('SELECT date_publication, contenu, publisher FROM article WHERE Id_article = :Id_article');
+            }
             $requeteId_article->execute(array(':Id_article' => $Id_article));
             $matchingData = $requeteId_article->fetchALL();
             $blob=array();
             $blob=json_encode($matchingData);
         }else{
-            $requeteArticles = $linkpdo->prepare('SELECT * FROM article');
+            if ($role = 'publisher' or $role = 'moderator') {
+                $requeteId_article = $linkpdo->prepare('select COUNT(Est_like) as Nombre_like, article.* 
+                                                        FROM interagir , article 
+                                                        where est_like = 1');
+            }else{
+                $requeteId_article = $linkpdo->prepare('SELECT date_publication, contenu, publisher FROM article');
+            }
             $requeteArticles->execute();
             $matchingData = $requeteArticles->fetchALL();
             $blob=array();
--- README.md
+++ README.md
@@ -2,11 +2,29 @@
 Projet de conception et développement d’API REST pour la gestion d’articles
 
 ## Informations utiles à l’évaluation de notre projet
-(ex : - URL d’accès à notre backend)
+(Ex : - URL d’accès à notre back-end)
 
-Contrainte :
-le moderator ne peut pas publier d'articles
-le publisher peut publier des articles, modifier ceux qu'il a publier
+## Documentation de notre API
+
+### Définition d'un article :
+Chaque article est défini par un ```IdArticle```, une ```date_de_publication```, un ```contenu``` et un ```publisher```.
+Un article peut être liké ou disliké par les publishers uniquement.
+
+### Définition d'un utilisateur :
+Chaque utilisateur est défini par un ```username```, un ```password``` et un ```role```.
+
+#### Contraintes de rôle :
+- Le ```moderator``` ne peut pas publier d'articles mais il peut les supprimer.
+- Le ```publisher``` peut publier des articles, modifier ceux qu'il a publier,
+et interagir avec les articles des autres publishers (like/dislike).
+- Une personne non connectée peut voir les articles mais ne peut pas interagir avec.
+
+### Fonctionnalités des méthodes HTTP:
+- ```GET``` : affiche tous les articles avec leur date et leur contenu (avec les rôles publisher et moderator, affiche également le publisher et le nombre de like et dislike)
+- ```GET``` avec ```?IdArticle=[IdArticle]``` : Affiche l'article pour IdArticle correspondant s'il existe
+- ```POST``` : publier un nouvel article (disponible avec le rôle publisher uniquement)
+- ```PUT``` avec ```?IdArticle=[IdArticle]``` : Modifier un article
+- ```DELETE``` avec ```?IdArticle=[IdArticle]``` : Supprimer un article (disponible avec les rôles publisher et moderator)
 
 ## Auteurs
 - Gaïa Ducournau
